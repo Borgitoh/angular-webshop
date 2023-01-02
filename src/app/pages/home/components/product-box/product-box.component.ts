28f2e7ab5da198fd59c672e393edc92ff0595c32
Kene Conde <kene.conde@jupiter.co.ao>
2023-01-02 16:24:23 +0100

    create products-box

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductBoxComponent implements OnInit {
 
+  @Input() fullWidthMode = false;
+  
   constructor() { }
 
   ngOnInit(): void {
